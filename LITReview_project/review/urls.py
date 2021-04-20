44c4cf368ad096b8433ce45478d0c3e41a4ba0a9
Emmanuel WALDNER <emmanuel.waldner@gmail.com>
2021-04-20 22:32:25 +0200

    Avancement Page d'accueil

--- LITReview_project/review/urls.py
+++ LITReview_project/review/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
+    path('ticket/', views.ticket, name='ticket'),
+    path('review/', views.review, name='review'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- LITReview_project/review/views.py
+++ LITReview_project/review/views.py
@@ -1,8 +1,13 @@
+from itertools import chain
+
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.views import LoginView
+
+from django.db.models import CharField, Value
 from .forms import Register, Connect
+from .models import Ticket, Review
 
 
 # Create your views here.
@@ -10,14 +15,51 @@ from .forms import Register, Connect
 class Login(LoginView):
     """ Class in order to change login form"""
 
-    templates_name = 'registration/login.html'
+    template_name = 'review/login.html'
     authentication_form = Connect
+    extra_context = {'title': ' - Accueil'}
+
+
+def get_users_viewable_reviews(user):
+    return Review.objects.filter(user=user)
+
+
+def get_users_viewable_tickets(user):
+    return Ticket.objects.filter(user=user)
 
 
 @login_required
 def index(request):
-    context = { 'test': 'context'}
+    reviews = get_users_viewable_reviews(request.user)
+    # returns queryset of reviews
+    reviews = reviews.annotate(content_type=Value('REVIEW', CharField()))
+
+    tickets = get_users_viewable_tickets(request.user)
+    # returns queryset of tickets
+    tickets = tickets.annotate(content_type=Value('TICKET', CharField()))
+
+    # combine and sort the two types of posts
+    posts = sorted(
+            chain(reviews, tickets),
+            key=lambda post: post.time_created,
+            reverse=True
+    )
+    context = {'title': ' - Flux',
+               'posts': posts}
+
     return render(request, 'review/index.html', context)
+
+
+@login_required
+def ticket(request):
+    context = {'title': ' - Demander une critique'}
+    return render(request, 'review/ticket.html', context)
+
+
+@login_required
+def review(request):
+    context = {'title': ' - Créer une critique'}
+    return render(request, 'review/review.html', context)
 
 
 def register(request):
@@ -32,6 +74,7 @@ def register(request):
         form = Register()
 
     context = {
+            'title': ""' - Inscription'"",
             'form': form
     }
     return render(request, 'review/register.html', context)
