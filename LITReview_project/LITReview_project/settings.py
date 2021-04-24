a9335dceab0bddaabc9fca77a4e6a802c595c7ec
Emmanuel WALDNER <emmanuel.waldner@gmail.com>
2021-04-24 21:46:18 +0200

    Index apres login fini. Images OK

--- LITReview_project/LITReview_project/settings.py
+++ LITReview_project/LITReview_project/settings.py
@@ -140,3 +140,6 @@ LOGIN_URL = '/login/'
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'review/img')
+MEDIA_URL = '/img/'
